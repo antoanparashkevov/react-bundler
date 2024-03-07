2b59f6609d53281db7667c9e558f598f11a828e0
“antoanparashkevov” <tonkata1505@gmail.com>
2024-03-07 21:05:28 +0200

    Serve /build folder as a static asset folder

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
 import { serve } from '@hono/node-server'
+import { serveStatic } from '@hono/node-server/serve-static';
 import { Hono } from 'hono'
 
 const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
-})
+});
 
-const port = 3000
-console.log(`Server is running on port ${port}`)
+app.use('/build/*', serveStatic());
 
-serve({
-  fetch: app.fetch,
-  port
+serve(app, async ({ port }) => {
+  console.log(`Server is running on port ${port}`)
 })
