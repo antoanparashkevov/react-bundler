896c2d33c814b35ccd04a7d68ed5cc8bd172a3f9
“antoanparashkevov” <tonkata1505@gmail.com>
2024-03-07 21:10:38 +0200

    Create two helper functions responsible for resolving app directory and build directory

--- src/index.ts
+++ src/index.ts
@@ -12,4 +12,21 @@ app.use('/build/*', serveStatic());
 
 serve(app, async ({ port }) => {
   console.log(`Server is running on port ${port}`)
-})
+});
+
+/** import.meta.url: file:///Users/tonkata1505/projects/react-bundler/src/index.ts */
+/** new URL("app/", import.meta.url): file:///Users/tonkata1505/projects/react-bundler/src/app/ */
+/** new URL("stream/", appBaseDir): file:///Users/tonkata1505/projects/react-bundler/src/app/stream */
+/** new URL("stream/", appBaseDir).pathname: /Users/tonkata1505/projects/react-bundler/src/app/stream */
+
+function resolveApp(path: string = "") {
+  const appBaseDir = new URL("app/", import.meta.url);
+
+  return new URL(path, appBaseDir).pathname;
+}
+
+function resolveBuild(path: string = "") {
+  const appBaseBuild = new URL("build/", import.meta.url);
+
+  return new URL(path, appBaseBuild).pathname;
+}
